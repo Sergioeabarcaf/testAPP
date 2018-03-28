711633ce66b4faa3bb579219c35d1c3a928b3140
sergioeabarcaf <sergioaf1991@gmail.com>
2018-03-27 23:51:52 -0300

    se agrega control via IBOutlet y IBAction para mostar imagen y titulo y esconder boton

--- testAPP/ViewController.swift
+++ testAPP/ViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tittleImageView: UIImageView!
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var buttonView: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tittleImageView.isHidden = true
+        self.backgroundImage.isHidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +31,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPresionado(_ sender: UIButton) {
+        self.backgroundImage.isHidden = !self.backgroundImage.isHidden
+        self.tittleImageView.isHidden = !self.tittleImageView.isHidden
+        self.buttonView.isHidden = true
+    }
+    
 }
 
